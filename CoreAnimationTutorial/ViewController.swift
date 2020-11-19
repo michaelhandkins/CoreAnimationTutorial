707d4bb083ead05f94019406194411e443e5e63d
Michael Handkins <michaelhandkins@gmail.com>
2020-11-19 10:28:10 -0600

    Add opacity change animation

--- CoreAnimationTutorial/ViewController.swift
+++ CoreAnimationTutorial/ViewController.swift
@@ -20,21 +20,35 @@ class ViewController: UIViewController {
         
         DispatchQueue.main.asyncAfter(deadline: .now()+3) {
             self.animateMovement()
+            self.animateOpacity()
         }
         
     }
     
     func animateMovement() {
-        
         let animation = CABasicAnimation(keyPath: "position")
-        animation.fromValue = CGPoint(x: layer.frame.origin.x, y: layer.frame.origin.y)
+        animation.fromValue = CGPoint(x: layer.frame.origin.x + (layer.frame.size.width/2),
+                                      y: layer.frame.origin.y + (layer.frame.size.height/2))
         animation.toValue = CGPoint(x: 300, y: 400)
-        animation.duration = 2
+        animation.duration = 1
+        //animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunction)
         animation.fillMode = .forwards
         animation.isRemovedOnCompletion = false
         animation.beginTime = CACurrentMediaTime()
         layer.add(animation, forKey: nil)
     }
+    
+    func animateOpacity() {
+        let animation = CABasicAnimation(keyPath: "opacity")
+        animation.fromValue = 1
+        animation.toValue = 0
+        animation.duration = 3
+        animation.fillMode = .forwards
+        animation.isRemovedOnCompletion = false
+        animation.beginTime = CACurrentMediaTime()
+        layer.add(animation, forKey: nil)
+    }
+
 
 
 }
